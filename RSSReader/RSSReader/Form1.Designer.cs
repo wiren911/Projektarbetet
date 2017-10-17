05c3ec9ac1722825947a405776c1993df9d23061
wiren911 <wirendavid@gmail.com>
2017-10-17 15:11:33 +0200

    hämtning av xml lista

--- RSSReader/RSSReader/Form1.Designer.cs
+++ RSSReader/RSSReader/Form1.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnGetXML = new System.Windows.Forms.Button();
+            this.lstRSS = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(24, 28);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(442, 20);
+            this.txtURL.TabIndex = 0;
+            // 
+            // btnGetXML
+            // 
+            this.btnGetXML.Location = new System.Drawing.Point(24, 72);
+            this.btnGetXML.Name = "btnGetXML";
+            this.btnGetXML.Size = new System.Drawing.Size(75, 23);
+            this.btnGetXML.TabIndex = 1;
+            this.btnGetXML.Text = "button1";
+            this.btnGetXML.UseVisualStyleBackColor = true;
+            this.btnGetXML.Click += new System.EventHandler(this.btnGetXML_Click);
+            // 
+            // lstRSS
+            // 
+            this.lstRSS.FormattingEnabled = true;
+            this.lstRSS.Location = new System.Drawing.Point(170, 83);
+            this.lstRSS.Name = "lstRSS";
+            this.lstRSS.Size = new System.Drawing.Size(314, 251);
+            this.lstRSS.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 345);
+            this.Controls.Add(this.lstRSS);
+            this.Controls.Add(this.btnGetXML);
+            this.Controls.Add(this.txtURL);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnGetXML;
+        private System.Windows.Forms.ListBox lstRSS;
     }
 }
 
--- RSSReader/RSSReader/Form1.cs
+++ RSSReader/RSSReader/Form1.cs
@@ -16,5 +16,18 @@ namespace RSSReader
         {
             InitializeComponent();
         }
+
+        private void btnGetXML_Click(object sender, EventArgs e)
+        {
+            //var urlInput = txtURL.Text;
+            var listRSS = lstRSS;
+            var xmlRSS = "";
+            using (var client = new System.Net.WebClient())
+            {
+                client.Encoding = Encoding.UTF8;
+                xmlRSS = client.DownloadString("http://bellobard.libsyn.com/rss");
+            }
+            
+        }
     }
 }
