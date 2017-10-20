2889699d148a380ac84125c2f55ed007e2ddcac0
wiren911 <wirendavid@gmail.com>
2017-10-20 09:39:36 +0200

    Hämtning presentation av xml-listan

--- RSSReader/dataHandler/Program.cs
+++ RSSReader/dataHandler/Program.cs
@@ -10,42 +10,28 @@ namespace DataHandler
 {
     class Program
     {
+       
 
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public string Description { get; set; }
-        public void Save (string fileName)
-        {
-            using (var stream = new FileStream(fileName, FileMode.Create))
-            {
-                var XML = new XmlSerializer(typeof(Program));
-                XML.Serialize(stream, this);
-            }
-        }
-
-        public void btnGetXML_Click(object sender, EventArgs e)
-        {
-            //var urlInput = txtURL.Text;
-            var xmlRSS = "";
-            using (var client = new System.Net.WebClient())
-            {
-                client.Encoding = Encoding.UTF8;
-                xmlRSS = client.DownloadString("http://bellobard.libsyn.com/rss.xml");
-            }
-            var dom = new System.Xml.XmlDocument();
-            dom.LoadXml(xmlRSS);
-
-            foreach (System.Xml.XmlNode item in dom.DocumentElement.SelectNodes("channel/item"))
-            {
-                var title = item.SelectSingleNode("title");
-                
-            }
-            
-        }
-        
         static void Main(string[] args)
         {
-           
+                var xml = "";
+                using (var client = new System.Net.WebClient())
+                {
+                    client.Encoding = Encoding.UTF8;
+                    xml = client.DownloadString("http://bellobard.libsyn.com/rss");
+                }
+                var dom = new System.Xml.XmlDocument();
+                dom.LoadXml(xml);
+
+                foreach(System.Xml.XmlNode item in dom.DocumentElement.SelectNodes("channel/item"))
+                {
+                    var title = item.SelectSingleNode("title");
+                    Console.WriteLine(title.InnerText);
+                }
+            Console.ReadKey();
         }
+        
     }
+  
+
 }
